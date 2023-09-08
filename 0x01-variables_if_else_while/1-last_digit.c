83c8db4961137a5fddca10faa2bc81a1d490b5ee
malikbreeze <abdulmalikidris680@gmail.com>
2023-09-08 11:35:47 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,14 @@
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
  * Return: Always 0 (success)
  */
+
+/* more headers go there*/
+
+/* betty style doc for function main goes there*/
 int main(void)
 {
 int n;
