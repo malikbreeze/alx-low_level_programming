48c12aac757d72f853cfb57f582237891b39918f
malikbreeze <abdulmalikidris680@gmail.com>
2023-09-14 16:24:17 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,9 +14,12 @@ void print_square(int size)
 
 	if (size <= 0)
 		_putchar('\n');
-	for (i = 0; i < (size); j++)
+	for (i = 0; i < (size); i++)
+	{
+	for (j = 0; j < (size); j++)
 	{
 		_putchar('#');
 	}
 	_putchar('\n');
+	}
 }
