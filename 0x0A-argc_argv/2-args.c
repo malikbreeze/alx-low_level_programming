f9bbc510aa288a0435cf4c07d611f3e29c99801f
malikbreeze <abdulmalikidris680@gmail.com>
2023-09-29 14:22:09 -0700

    2-args.c.

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,6 +3,7 @@
  * main - print all arguments it recieve
  * @argc: number of command line arguments
  * @argv: array that contains the command line argument
+ * Return: (0) when successful
  */
 int main(int argc, char *argv[])
 {
