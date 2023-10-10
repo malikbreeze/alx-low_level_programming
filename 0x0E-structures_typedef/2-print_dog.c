ac579d323339c3d40d814b4c20ab7da7c9ab8f2c
malikbreeze <abdulmalikidris680@gmail.com>
2023-10-10 14:59:07 -0700

    updates_2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,7 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (!((d->name))
+		if (!(d->name))
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
@@ -17,6 +17,6 @@ void print_dog(struct dog *d)
 		if (!(d->owner))
 			printf("Owner: (nil)\n");
 		else
-			printf("Owner %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }
