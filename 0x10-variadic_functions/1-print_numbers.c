2f8d247789cd7f81c301f2692a0cb5906e740714
malikbreeze <abdulmalikidris680@gmail.com>
2023-10-12 14:25:30 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 /**
  * print_numbers - print integers
- * @seperator: string to be printed between numbers
+ * @separator: string to be printed between numbers
  * @n: number of ints to print
  * Return: void
  */
