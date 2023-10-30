49baec5a054865f76d8750d92961cb0b6d83cbca
malikbreeze <abdulmalikidris680@gmail.com>
2023-10-30 10:03:20 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int file, result = 1, countn = 0;
 
 	if (!filename)
-		return(-1);
+		return (-1);
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
